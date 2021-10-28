e72195bb5c2a9cf8f9711124ad378221a17ecefe
AWS ParallelCluster user <ec2-user@ip-10-0-0-253.ec2.internal>
2021-10-28 01:04:16 +0000

    Working version of transformers without freezing layers, and with bigger seq length

--- source_using_transformers/run_3epochs_using_transformer.sh
+++ source_using_transformers/run_3epochs_using_transformer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#!/bin/bash
-#SBATCH --job-name=run_3_epochs_asc_original.out
+#SBATCH --job-name=run_3_epochs_transformer.out
 #SBATCH --output=slurm-%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --exclusive
 
 #CUDA_VISIBLE_DEVICES=0 pipenv run python run_race.py  --data_dir=../RACE --vocab_file=../pretrained_model_asc/30k-clean.vocab --spm_model_file=../pretrained_model_asc/30k-clean.model --config_file=../pretrained_model_asc/config.json  --bert_model=../pretrained_model_asc/pytorch_model.bin --output_dir=asc_models --max_seq_length=320  --do_train --do_eval --do_lower_case --train_batch_size=2 --eval_batch_size=1 --learning_rate=1e-5 --num_train_epochs=1 --gradient_accumulation_steps=1 --fp16 --loss_scale=64
-export CUDA_VISIBLE_DEVICES=0
-time pipenv run python run_race.py  --data_dir=../RACE --vocab_file=../pretrained_model_asc/30k-clean.vocab --spm_model_file=../pretrained_model_asc/30k-clean.model --config_file=../pretrained_model_asc/config.json  --bert_model=../pretrained_model_asc/pytorch_model.bin --output_dir=asc_models --max_seq_length=360  --do_train --do_eval --do_lower_case --train_batch_size=2 --eval_batch_size=1 --learning_rate=1e-5 --num_train_epochs=3 --gradient_accumulation_steps=1 --fp16 --loss_scale=64
+export CUDA_VISIBLE_DEVICES=0 
+time pipenv run python run_race.py  --data_dir=../RACE --vocab_file=../pretrained_model_asc/30k-clean.vocab --spm_model_file=../pretrained_model_asc/30k-clean.model --config_file=../pretrained_model_asc/config.json  --bert_model=../pretrained_model_asc/pytorch_model.bin --output_dir=asc_models --max_seq_length=384  --do_train --do_eval --do_lower_case --train_batch_size=2 --eval_batch_size=1 --learning_rate=1e-5 --num_train_epochs=3 --gradient_accumulation_steps=1 --fp16 --loss_scale=64
